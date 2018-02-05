8078c70081819f03f1ffa0ece1e19d8bdd7493af
Rain-Fly <diaoyufei@hotmail.com>
2018-02-05 11:28:19 +0800

    Initial commit

---
+++ src/main/java/org/hqu/production_ms/domain/vo/TechnologyPlanVO.java
@@ -0,0 +1,18 @@
+package org.hqu.production_ms.domain.vo;
+
+
+import org.hqu.production_ms.domain.TechnologyPlan;
+
+public class TechnologyPlanVO extends TechnologyPlan{
+	
+	private String technologyName;
+
+	public String getTechnologyName() {
+		return technologyName;
+	}
+
+	public void setTechnologyName(String technologyName) {
+		this.technologyName = technologyName;
+	}
+	
+}
